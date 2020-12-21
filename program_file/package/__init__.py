bc6c826eb9941ead820323916df8dfd15f386b3f
massao000 <69783019+massao000@users.noreply.github.com>
2020-12-21 13:38:04 +0900

    Delete __init__.py

--- program_file/package/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import random
-import time
-import csv
-from datetime import date
-from colorama import Fore, Back, Style, init
-from package.operator import Writing, Operator, IntError, Animation
-from package.color import Color, Escape
